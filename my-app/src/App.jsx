1e10213b757d809896ee2a104c7bf0c830ab0c22
j._.jinoo <jinoo0306@naver.com>
2024-02-14 00:19:49 +0900

    practice : react에서 tailwind css 연습

---
+++ my-app/src/App.jsx
@@ -0,0 +1,12 @@
+import "./index.css";
+import "./styles/global.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="m-6 bg-yellow-500 text-blue-500">hello world</h1>
+    </div>
+  );
+}
+
+export default App;
